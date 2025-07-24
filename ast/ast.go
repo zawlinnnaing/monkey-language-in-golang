6ec191d4c96589dfa56dda2c8585da3356ddf7da
Zaw <zawlinnnaing0018@gmail.com>
2025-07-24 16:42:04 +0700

    feat: added AST nodes

---
+++ ast/ast.go
@@ -0,0 +1,42 @@
+package ast
+
+import "github.com/zawlinnnaing/monkey-language-in-golang/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (id *Identifier) expressionNode() {}
+func (id *Identifier) TokenLiteral() string {
+	return id.Token.Literal
+}
